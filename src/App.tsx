c14cc9ceb90929e3b1784bb00fa63969037c5fbd
jvictorb <jr-851@hotmail.com>
2025-01-30 17:36:36 -0300

    Adicionando MaterialUi ao Projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 export const App = () => {
   return (
