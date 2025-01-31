ad05341569bcc410b2e056a95f7474d43a13134f
jvictorb <jr-851@hotmail.com>
2025-01-31 12:02:30 -0300

    feat: adicionado themas

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,17 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { ThemeProvider } from "@mui/material";
+import { LightTheme } from "./shared/themes";
 
 
 export const App = () => {
   return (
+    <ThemeProvider theme={LightTheme}>
     <BrowserRouter>
     <AppRoutes></AppRoutes>
     </BrowserRouter>
+    </ThemeProvider>
   );
+
 }
